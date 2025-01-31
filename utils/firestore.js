9439e1d1723fd7b1be41d05e769dddd69f35efb5
Undiluted7027 <undiluted7027@protonmail.com>
2025-01-30 19:06:14 -0800

    Added Users model schema, needs linking and reference links for following and posts, test worked, need to add more test cases and a testing suite #12

--- utils/firestore.js
+++ utils/firestore.js
@@ -1,4 +1,4 @@
-import { getFirestore, collection, addDoc } from "@firebase/firestore";
+import { getFirestore, collection, doc, setDoc, updateDoc } from "@firebase/firestore";
 import firebaseApp from "./firebaseConfig";
 
 // Connect to database
@@ -8,12 +8,24 @@ console.log("Database Connected!");
 export const addData = async (collectionName, data) => {
   // Accepts name of collection (table) and data (object with key value pairs)
   // Adds data to firebase
+  const newDataRef = doc(collection(db, collectionName));
+  data.id = newDataRef.id;
   try {
-    const docRef = await addDoc(collection(db, collectionName), data);
-    console.log("Document written with ID: ", docRef.id);
+    await setDoc(newDataRef, data);
+    console.log("Document written with ID: ", newDataRef.id);
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 };
+
+export const updateData = async(collectionName, data) => {
+  const docRef = doc(collection(db, collectionName), data.id);
+  try{
+    await updateDoc(docRef, data);
+    console.log(`Document with id ${data.id} which is a part of ${collectionName} was updated.`);
+  } catch(e){
+    console.error("Error updating the document: ", e);
+  }
+}
 
 export default db;
